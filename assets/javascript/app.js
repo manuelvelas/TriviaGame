477f9b5077a175914fa27139708bc2f0705ab1db
mevpadrino <manuelman.mev@gmail.com>
2019-06-06 23:23:45 -0500

    still need work, some changes made but the logic is not working properly, I expect to make some improvement to having running soon

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,15 +2,18 @@
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
-var initialTime = 60;
+var initialTime = 10;
 var totalScore = 0;
 
 var myObject = {
     firstPropperty: 'stuff'
 }
 
-console.log(myObject.firstPropperty)
 
+
+console.log(myObject.firstPropperty)
+// our variable questions hold the questions, answers, and and correct answer, 
+// also includes an image of the player
 var questions = [
     {
         question: 'this is question 1',
@@ -20,8 +23,8 @@ var questions = [
     },
     {
         question: 'this is question 2',
-        choices: [' hola ', ' sayonara '],
-        correctAnswer: 'choice 1',
+        choices: ['hola ', ' sayonara '],
+        correctAnswer: 'hola',
         image: 'assets/images/ronaldo.jpeg',
     },
     {
@@ -32,31 +35,141 @@ var questions = [
     }
 ]
 
-// Functions
+// Function start coundown take cares of the time
 var startCountdown = function() {
     setInterval(function() {
         // initialTime = initialTime - 1
         initialTime--;
         // console.log(initialTime)
-        $('#clock').text(initialTime)
+        $('#clock').text("you Have "+initialTime+ " Seconds Left");
     }, 1000)
 }
 
 // Dynamically append html with jquery
 // with this for loop we will display the questions an answers
+//funtion renderquestion give us the question an answers in the webpage
+function renderquestion(currentQuestion){
+    
+    var i = currentQuestion;
+    var questiones = questions[i].question;
+    $("#fotos").html("<img src=" + questions[i].image + " width='400px'>");
+    $("#questions").html("<div class='list-group-item'>"+ questiones + "</div>");
+    for(var j = 0; j < questions[i].choices.length; j++) {
+        $("#answ").append("<div class='btn btn-light'>"+ questions[i].choices[j] + "</div>");
+        
+          
+      }  
+      $("#answ").click(function(event) {
+        var userguess = event.target.textContent;
+    //  console.log(event.target.textContent);
+       console.log(userguess);
+       
+    });    
+    i++;
+}
 
-function renderquestion(){
+// function reset(){
+//     initialTime =10;
 
-for (i = 0; i <= questions.length -1; i++) {
-    var questiones = document.querySelector("#questions").text = questions[i].question;
-    // var questiones = questions[i].question;
-    var respuesta = document.querySelector("#answ").text = questions[i].choices;
-    // var respuesta = questions[i].choices;
-    $("#image-holder").html("<img src=" + questions[i].image + " width='400px'>");
-    $("#questions").text(questiones);
-    $("#answ").text(respuesta);
-    console.log(questions[i].question);
-    console.log(respuesta);
+// }
+
+// function answerCheck(){
+
+//     var chose;
+//     $("answ").on("click", function() {
+//         chose = questions[i].choices;
+//         console.log(chose);
+
+   
+//       }); 
+// }
+
+// main funtion runs everything 
+function mainFunction() {
+    
+    startCountdown();
+    var currentQuestion = 0;
+    renderquestion(currentQuestion);
+      
+   
+  
+    // $("#answ").click(function(event) {
+    //     var userguess = event.target.textContent;
+    // //  console.log(event.target.textContent);
+    //    console.log(userguess);
+       
+    // });   
+    
+for(var k=0; k < questions.length; k++){
+    // if(currentQuestion < questions.length){
+        var userguess = questions[k].choices;
+        
+    if(userguess === questions.correctAnswer && initialTime >= 0){
+
+        correct ++;
+        currentQuestion ++;
+        initialTime = 10
+        console.log(correct);
+        console.log(currentQuestion);
+        
+
+
+
+    // if they choose wrong
+    // increment wrongAnswer counter
+    }else if(userguess != questions.correctAnswer && initialTime >= 0){
+
+        incorrect ++;
+        currentQuestion++
+        initialTime = 10
+        console.log(correct);
+        console.log(currentQuestion);
+       
+
+     
+
+    }else if(initialTime <= 0){
+        unanswered ++;
+        console.log(unanswered);
+
+    }
+
+    // }
+
+    }
+}
+mainFunction();
+
+// intial start trivia functions
+
+
+
+// 
+
+// renderQuestion
+// add onClick to the choices
+// check if the answer they clicked on is the correct answer
+
+
+// if time is up
+    // then act like they choose the wrong answer
+
+// render the next Question 
+// currentQuestion++;
+
+// if no more questions
+
+
+
+
+
+
+
+
+
+
+
+
 // // questions.forEach(function(element, i) {
 //     var question = questions[i].question;*
 //     var respuesta = questions[i].choices;*
@@ -77,14 +190,3 @@ for (i = 0; i <= questions.length -1; i++) {
 //     // $("#cardtitle").text(pregunta);
 //     // $('#questions').append(div, [divCard, [pregunta]])
 // // })
-}
-}
-
-
-// Event Listeners
-
-
-
-// intial start trivia functions
-startCountdown();
-renderquestion();
